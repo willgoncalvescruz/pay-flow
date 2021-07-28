d3e76815dccef2d84c790be504e182ee54910aa6
Will Gonçalves <wilsinhogc@gmail.com>
2021-07-27 23:43:32 -0300

    Primeiro Commit PayFlow

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/app_widget.dart';
+
+void main() {
+  runApp(AppFireBase());
+}
+
+class AppFireBase extends StatefulWidget {
+  @override
+  State<AppFireBase> createState() => _AppFireBaseState();
+}
+
+class _AppFireBaseState extends State<AppFireBase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Material(
+              child: Center(
+                child: Text(
+                  "Não foi possível inicializar o Firebase",
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return AppWidget();
+          } else {
+            return Material(
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
+  }
+}
---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUSer(context);
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull)),
+        ],
+      ),
+    );
+  }
+}
